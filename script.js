db5df7279f9d32225e9065d4e554bd57bf93f708
Shizcow <pohl.devin@gmail.com>
2019-09-02 10:19:31 -0600

    class, let, and const actually work as expected now, with value passthroughs and everything

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 function stylize_code(code){
     // this should be itterative so we don't need 4 million wrappers
     var tokens = []; // tokenizer
-
     for(let i = 0; i<code.length; ++i){
 	var token = "";
 	if(token = code.substr(i).match(/^\/\*[\s\S]*?\*\/|^\s*\/\/.*\s*/)){ // single line comment
@@ -42,25 +41,24 @@ function stylize_code(code){
 	    boolWrapper.style.color = "#E170CF";
 	    tokens.push(boolWrapper);
 	    i += boolWrapper.innerText.length-1;
-	} else if(token = code.substr(i).match(/^(\s*[A-Za-z\$\_][A-Za-z1-9\$\_]*\s*)+/)){ // identifier
-	    token = token[0];
+	} else if(token = code.substr(i).match(/^(\s*(?!(true|false|null|undefined|class|function|let|var|return|yield)(?![A-Za-z1-9\$\_]))[A-Za-z\$\_][A-Za-z1-9\$\_]*\s*)+/)){ // identifier - filter out keywords
 	    var identifier = document.createElement('span');
-	    identifier.innerText = token;
+	    identifier.innerText = token[0];
 	    identifier.style.color = "#906FC7";
 	    tokens.push(identifier);
-	    i += identifier.innerText.length;
+	    i += token[0].length;
 	    while(token = code.substr(i).match(/^(\s*\.\s*)+/)){ // property
 		var point = document.createElement('span');
 		point.innerText = token[0];
 		point.style.color = "#D7D7DB";
 		tokens.push(point);
 		i += point.innerText.length;
-		if(token = code.substr(i).match(/^\s*[A-Za-z\$\_][A-Za-z1-9\$\_]*\s*/)){ // following property, if applicable - no expansion
+		if(token = code.substr(i).match(/^\s*(?!(true|false|null|undefined|class|function|let|var|return|yield)(?![A-Za-z1-9\$\_]))[A-Za-z\$\_][A-Za-z1-9\$\_]*\s*/)){ // following property, if applicable - no expansion
 		    var property = document.createElement('span');
 		    property.innerText = token[0];
 		    property.style.color = "#86DE74";
 		    tokens.push(property);
-		    i += property.innerText.length;
+		    i += token[0].length;
 		}
 	    }
 	    --i;
@@ -233,41 +231,60 @@ document.getElementById("codeInput").onkeydown = function(e){
 	
 	hist = 0;
 
-	preFormattedCode = code; // don't show the user our hacks
+	let preFormattedCode = code;
 
-	// before we do literally anything, we need to parse the input a bit
-	// reason being: classes don't declare in evals
-	// so we need to set window.className = className
-	// but only on the global level, IE if not wrapped in any amount of parens
-	for(let i=0, paren_level=0; i<code.length; ++i){
-	    if(code[i]=="{")
-		paren_level++;
-	    if(code[i]=="}")
-		paren_level--;
-	    if(!paren_level && code.substr(i, 6) == "class " && (i > 0 ? code[i-1].match(/^[\s\}\(\)\[\];\,\:]/) : 1)){
-		let j = code.slice(i).indexOf("{");
-		if(j == -1)
-		    break; // missing paren error
-		className = code.substr(i+6).split("{")[0];
-		j = code.slice(i).indexOf("{")+i+1;
-		for(let parens = 1; parens > 0;){
-		    if(code[j] == undefined)
-			break; // unmatched paren
-		    iOpen = code.slice(j).indexOf("{");
-		    iClosed = code.slice(j).indexOf("}");
-		    if(iOpen == -1 || iClosed < iOpen){
-			parens--;
-			j += iClosed+1;
-		    } else {
-			parens++;
-			j += iOpen+1;
-		    }
+	// first, use esprima to grab all varibles that should be global at the end and need some
+	// help getting into the proper scope
+	let names = [];
+	let error = false
+	let parsed;
+	try{
+	    parsed = esprima.parseScript(code, {range: true});
+	} catch (e){
+	    error = true; // so errors work
+	}
+	for(let i=0; !error&&i<parsed.body.length;++i){
+	    if(parsed.body[i].type == "VariableDeclaration" && parsed.body[i].kind == "let" || parsed.body[i].kind == "const")
+		for(let j=0; j<parsed.body[i].declarations.length; ++j)
+		    names.push({type: parsed.body[i].kind, name: parsed.body[i].declarations[j].id.name});
+	    else if(parsed.body[i].type == "ClassDeclaration")
+		names.push({type: "class", name: parsed.body[i].id.name});
+	}
+	
+	// problem: this means the last line doesnt give the right return value
+	// we need a way to pass through any return value, past the next statement
+
+	// need to be injected immediatly after varible declaration, and have an extra varible line
+	// esprima can give locations of varible definitions (Line and column-based or Index-based range)
+
+	// idea: get the index range of the LAST global declaration (of any type). That will guarentee a safe spot to put a semicolon and inject Object.defineProperties - plural, then add the name after again
+	if(names.length){
+	    finalDeclaration = parsed.body.filter(el => el.type.includes("Declaration")).slice(-1)[0];
+	    if(finalDeclaration.declarations)
+		finalDeclaration = finalDeclaration.declarations.slice(-1)[0];
+	    if(finalDeclaration){
+		properties = "Object.defineProperties(window, {"
+		for(let i=0; i<names.length; ++i){
+		    properties += names[i].name + ": {enumerable: true, configurable: false, get: function(){return " + names[i].name + ";}, set: function(val){";
+		    if(names[i].type == "const")
+			properties += "throw new TypeError(\"TypeError: invalid assignment to const `" + names[i].name + "'\")";
+		    if(names[i].type == "let" || names[i].type == "class")
+			properties += "return " + names[i].name + " = val;"
+		    properties += "}}";
+		    if(i < names.length-1)
+			properties += ",";
 		}
-		code = code.slice(0, i) + "(function(){" + code.slice(i, j) + "; return window." + className + " = " + className + ";}())" + code.slice(j);
-		i = j;
+		properties+= "})"
+		code = code.substr(0, finalDeclaration.range[1]) + ";" + properties + ";" + finalDeclaration.id.name + ";" + code.substr(finalDeclaration.range[1])
 	    }
 	}
-
+	
+	//     ex: let a = 0; Object.define...; a;
+	// Object.defineProperty(window, "a", {below v})
+	// for const: {enumerable: true, configurable: false, get: function(){return a;}, set: function(val){console.error("TypeError: invalid assignment to const `a'")}}
+	// for let: {enumerable: true, configurable: false, get: function(){return a;}, set: function(val){return a = val;}}
+	// class is same as let
+	
 	// also, redirect console.log to our terminal emulator
 	// this means we need to start building divs first
 
@@ -280,8 +297,9 @@ document.getElementById("codeInput").onkeydown = function(e){
 	var resultPointer = document.createElement('label');
 	resultPointer.className = "resultPointer";
 	resultPointer.innerText = ">>";
-	
+
 	var evaluated = stylize_code(preFormattedCode);//document.createElement('span');
+	//var evaluated = stylize_code(code);//document.createElement('span');
 	evaluated.className = "evaluated";
 	
 	evaluatedWrapper.appendChild(resultPointer);
@@ -311,7 +329,6 @@ document.getElementById("codeInput").onkeydown = function(e){
 		topWrapper.removeChild(topWrapper.firstChild);
 	}
 	var result;
-	error = false;
 	try{
 	    result = (1, eval)(code);
 	} catch(e) {
